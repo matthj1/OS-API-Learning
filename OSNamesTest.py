19d7fda21a0208636b2266902882fb4e6e974735
Joe Matthews <joematthewsphotography@gmail.com>
2020-09-25 15:20:04 +0100

    also messed with find nearest in OS Names API

--- OSNamesTest.py
+++ OSNamesTest.py
@@ -1,11 +1,11 @@
 import requests
 
-BASE = "https://api.os.uk/search/names/v1/"
 KEY = "8sxgVXPjDNz4gW6FtBaATGVgqdWBqAEQ"
 
 
 def find(search_term, feature):
-    find_uri = BASE + "find?query=" + search_term + "&key=" + KEY
+    base = "https://api.os.uk/search/names/v1/"
+    find_uri = base + "find?query=" + search_term + "&key=" + KEY
     response = requests.get(find_uri)
     data = response.json()
     for results in data["results"]:
@@ -15,5 +15,26 @@ def find(search_term, feature):
     return search_term + " does not have a " + feature
 
 
+def to_national_grid(lat_long):
+    lat = lat_long[0]
+    long = lat_long[1]
+    URI = f"http://webapps.bgs.ac.uk/data/webservices/CoordConvert_LL_BNG.cfc?method=LatLongToBNG&lat={lat}&lon={long}"
+    lat_long_response = requests.get(URI)
+    data = lat_long_response.json()
+    return round(data["EASTING"]), round(data["NORTHING"])
+
+
+def nearest(coords, isBNG):
+    if isBNG:
+        BNG_coords = coords
+    else:
+        BNG_coords = to_national_grid(coords)
+    URI = f"https://api.os.uk/search/names/v1/nearest?point={BNG_coords[0]},{BNG_coords[1]}&radius=1000&key=" + KEY
+    response = requests.get(URI)
+    print(response.json())
+
+
 if __name__ == "__main__":
-    print(find("Portsmouth", "Harbour"))
+    #print(find("Portsmouth", "Harbour"))
+    to_national_grid((52.342622, -0.074505))
+    nearest((52.342622, -0.074505), False)
